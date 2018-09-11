90cf8e9ed67c3191108aeedea719bb4122ec6639
Jorge Cardenas <jorge.cardenas9006@gmail.com>
2018-09-11 10:45:17 -0500

    actualizacion

---
+++ src/app/models/producto.ts
@@ -0,0 +1,8 @@
+export class Producto{
+    id?: string;
+    betSeller?: boolean;
+    nombre?: string;
+    descripcion?: string;
+    precio?: string;
+    imagen?: string;
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ProductoProvider } from '../../providers/producto/producto';
+import { Producto } from '../../app/models/producto';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  productos: Producto[] = [];
+  constructor(public navCtrl: NavController,
+  private servicioProductos:ProductoProvider
+  ) {
 
-  constructor(public navCtrl: NavController) {
-
+  }
+  ionViewDidLoad(){
+    this.servicioProductos.obtenerProductos().subscribe(
+    (datos: Producto[]) => {
+      this.productos = datos;
+    }
+    );
+  }
+  irPaginaDetalle(producto){
+    this.navCtrl.push('DetalleProductoPage',{
+      productoDetalle: producto
+    });
   }
 
 }
